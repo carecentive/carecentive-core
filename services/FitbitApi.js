8fae5b128908237bcefce2243f4a8a42a2fdc01f
Syed Ikhtiar Ahmed <ikhtiar0240@gmail.com>
2023-10-20 22:53:12 +0200

    implement refresh token

--- services/FitbitApi.js
+++ services/FitbitApi.js
@@ -6,12 +6,44 @@ class FitbitApi {
         try {
             // Define the API endpoint and request payload
             const apiUrl = 'https://api.fitbit.com/oauth2/token';
-            
+
             const data = new URLSearchParams();
             data.append('client_id', process.env.FITBIT_CLIENT_ID);
             data.append('code', authorizationCode);
             data.append('code_verifier', process.env.FITBIT_CODE_VERIFIER);
             data.append('grant_type', 'authorization_code');
+
+            // Define request headers
+            const headers = {
+                'Authorization': 'Basic ' + process.env.FITBIT_BASIC_TOKEN,
+                'Content-Type': 'application/x-www-form-urlencoded',
+            };
+
+            const response = await axios({
+                method: "post",
+                url: apiUrl,
+                data: data,
+                headers: headers
+            });
+
+            if (response.status == "200") {
+                return response.data
+            } else {
+                throw new FitbitApiError(response.status + " (" + response.data.errors + ")")
+            }
+        } catch (error) {
+            throw error
+        }
+    }
+
+    static async refreshToken(currentRefreshToken) {
+        try {
+            // Define the API endpoint and request payload
+            const apiUrl = 'https://api.fitbit.com/oauth2/token';
+
+            const data = new URLSearchParams();
+            data.append('grant_type', 'refresh_token');
+            data.append('refresh_token', currentRefreshToken);
 
             // Define request headers
             const headers = {
--- services/FitbitDataHub.js
+++ services/FitbitDataHub.js
@@ -2,6 +2,8 @@ const FitbitApi = require('./FitbitApi')
 const FitbitToken = require('../models/FitbitToken')
 const { getDatetimeString } = require('../source/Utils');
 
+var logger = require('winston');
+
 class FitbitDataHub {
     static async registerUser(authorizationCode, userId) {
         try {
@@ -45,6 +47,54 @@ class FitbitDataHub {
         } catch (error) {
             console.error(error)
             throw error;
+        }
+    }
+
+    static async refreshUserToken(userId) {
+        try {
+            // Check if user was initially registered for the use with Fitbit API
+            let userFitbitTokenRecord = await FitbitToken.query().findOne({
+                user_id: userId
+            })
+
+            if (!userFitbitTokenRecord) {
+                throw new UserTokenNotFoundError;
+            }
+
+            // Get the refresh token through the Fitbit API Wrapper
+            const { access_token: accessToken, expires_in: expiresIn, refresh_token: refreshToken, token_type: tokenType, user_id: fitbitUserId } = await FitbitApi.refreshToken(userFitbitTokenRecord.refresh_token)
+
+            // Calculate new timestamps
+            let now = new Date();
+            let expirationDate = new Date
+            expirationDate.setUTCSeconds(expirationDate.getUTCSeconds() + expiresIn);
+
+            let nowDatetimeString = getDatetimeString(now)
+            let expirationDateDatetimeString = getDatetimeString(expirationDate)
+
+            // Insert updated user data into database
+            await userFitbitTokenRecord.$query().patchAndFetch({
+                fitbit_user_id: fitbitUserId,
+                token_last_updated: nowDatetimeString,
+                access_token: accessToken,
+                expiration_date: expirationDateDatetimeString,
+                refresh_token: refreshToken
+            })
+
+            return userFitbitTokenRecord
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    static async dataPollBatchUser(userId) {
+        try {
+            let tokenData = await this.refreshUserToken(userId)
+            console.log("Token Data:", tokenData)
+        }
+        catch (err) {
+            logger.error("Could not refresh access token for user " + userId + ": ", err, JSON.stringify(err));
+            return;
         }
     }
 }
