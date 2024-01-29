b8d13bd0ce5d2cf973e2d9b6913ab32686fb0c26
Sijan Shrestha <dsijan95@gmail.com>
2024-01-29 19:41:02 +0100

    Added Table schema and Model

---
+++ models/GoogleUserModel.js
@@ -0,0 +1,22 @@
+const { Model } = require("objection");
+
+const User = require("@carecentive/carecentive-core/models/User");
+
+class GoogleUser extends Model {
+  static get tableName() {
+    return "google_users";
+  }
+
+  static relationMappings = {
+    user: {
+      relation: Model.BelongsToOneRelation,
+      modelClass: User,
+      join: {
+        from: "google_users.user_id",
+        to: "user.id",
+      },
+    },
+  };
+}
+
+module.exports = GoogleUser;
