14e0609a6629c63eeee7bee0fcd763837a673b1e
Michael Nissen <53198659+michaeljnissen@users.noreply.github.com>
2024-05-02 16:16:07 +0200

    Google API dependency bugfix, database bugfix

--- database/migrations/20231112123146_fitbit_data_tables.js
+++ database/migrations/20231112123146_fitbit_data_tables.js
@@ -1,14 +1,14 @@
 exports.up = function (knex) {
 	return Promise.all([
 		knex.schema.createTable("user_fitbit_data", function(table) {
-            table.increments("id").primary();
+			table.increments("id").primary();
 			table.integer("user_id").unsigned().notNullable();
 			table.foreign("user_id").references("users.id");
 			table.string("request_type");
 			table.timestamp("request_timestamp");
-			table.timestamp("from_timestamp");
-			table.timestamp("to_timestamp");
-            table.text("response", "mediumtext");
+			table.timestamp("from_timestamp").nullable();
+			table.timestamp("to_timestamp").nullable();
+			table.text("response", "mediumtext");
 			table.timestamps(false, true);
 		})
 	]);
